0b8443f009af55994eab0df37a00a23346d93728
Carl Andreas Sveinsson <carl13@ru.is>
2015-02-24 16:35:59 +0000

    Working on scroll at home

--- client/js/scroll.js
+++ client/js/scroll.js
@@ -1,3 +1,10 @@
 $("#scroll").click(function() {
+	/*
+	var psconsole = $('#one');
+    if(psconsole.length)
+       psconsole.scrollTop(psconsole[0].scrollHeight - psconsole.height());
+	   
+	 */
+	 $("#myDiv").attr({ scrollTop: $("#myDiv").attr("scrollHeight") });
 	console.log("Gaur");
-	});
+});
